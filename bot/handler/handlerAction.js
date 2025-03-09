dc35d7aa90237b02cedb1e55311617538a7d1441
kamrulhasan898 <chotokamuai099@gmail.com>
2025-03-09 19:33:15 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "🙂"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61561101500902"){
+        if(event.reaction == "😾"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61552930114349"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
