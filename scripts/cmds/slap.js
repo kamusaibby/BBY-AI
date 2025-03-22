3545dafe49d9df7143bf624e6308df75932f68ac
kamusaibby <ark966434@gmail.com>
2025-03-22 21:41:38 +0600

    Create slap.js

---
+++ scripts/cmds/slap.js
@@ -0,0 +1,51 @@
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
+
+module.exports = {
+	config: {
+  name: "slap",
+  version: "1.1",
+  author: "NTKhang",
+  countDown: 5,
+  role: 0,
+  shortDescription: "Batslap image",
+  longDescription: "Batslap image",
+  category: "fun",
+  guide: {
+   en: "   {pn} @tag"
+  }
+	},
+
+	langs: {
+  vi: {
+   noTag: "Bạn phải tag người bạn muốn tát"
+  },
+  en: {
+   noTag: "You must tag the person you want to slap"
+  }
+	},
+
+	onStart: async function ({ event, message, usersData, args, getLang }) {
+  const uid1 = event.senderID;
+  const uid2 = Object.keys(event.mentions)[0];
+  
+  // Check if the mentioned user is the restricted ID
+  if (uid2 === "100078140834638" || uid2 === "100084690500330") { 
+    return message.reply("Slap yourself Dude 🐸");
+  }
+
+  if (!uid2)
+   return message.reply(getLang("noTag"));
+   
+  const avatarURL1 = await usersData.getAvatarUrl(uid1);
+  const avatarURL2 = await usersData.getAvatarUrl(uid2);
+  const img = await new DIG.Batslap().getImage(avatarURL1, avatarURL2);
+  const pathSave = `${__dirname}/tmp/${uid1}_${uid2}Batslap.png`;
+  fs.writeFileSync(pathSave, Buffer.from(img));
+  const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
+  message.reply({
+   body: `${(content || "Bópppp 😵‍💫😵")}`,
+   attachment: fs.createReadStream(pathSave)
+  }, () => fs.unlinkSync(pathSave));
+	}
+};
