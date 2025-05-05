6ff621ffca14297e107a3c0d84548ee213a3e38a
kamusaibby <ark966434@gmail.com>
2025-05-05 09:22:21 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😾"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61552930114349"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61552536510387"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
